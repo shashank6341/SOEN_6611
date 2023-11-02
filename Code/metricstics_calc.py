0de92b9a4c8ce2b39e4e737126f36052f54769a3
Shashank Verma <8446697+shashank6341@users.noreply.github.com>
2023-11-02 14:50:15 -0400

    Added Minimum, Maximum and Mode Functionality.
    - Added logic for Minimum.
    
    - Added logic for Maximum.
    
    - Added logic for Mode.

--- Code/metricstics_calc.py
+++ Code/metricstics_calc.py
@@ -32,7 +32,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title("Input integers and floats")
+        self.title("METRICSTICS System.")
         self.geometry("300x100")
 
         self.entry = CustomEntry(self, placeholder="Ex: 1,45,57")
@@ -40,10 +40,60 @@ class App(tk.Tk):
         
         self.bind("<Button-1>", self.on_window_click)
 
+        # Result Label.
+        self.result_label = tk.Label(self, text="Result: ")
+        self.result_label.pack(padx=10, pady=5)
+
+
+        # Calculation Buttons.
+        self.min_button = tk.Button(self, text="Minimum", command=self.find_minimum)
+        self.min_button.pack(padx=10, pady=5, side=tk.LEFT)
+
+        self.max_button = tk.Button(self, text="Maximum", command=self.find_maximum)
+        self.max_button.pack(padx=10, pady=5, side=tk.LEFT)
+
+        self.mode_button = tk.Button(self, text="Mode", command=self.find_mode)
+        self.mode_button.pack(padx=10, pady=5, side=tk.LEFT)
+
+
     def on_window_click(self, event):
         widget = self.winfo_containing(event.x_root, event.y_root)
         if widget != self.entry:
             self.focus_set()
+
+    def _get_numbers(self):
+        try:
+            numbers = [float(num.strip()) for num in self.entry.get().split(',') if num]
+            return numbers
+        except ValueError:
+            tk.messagebox.showerror("Error", "Please enter valid comma-separated numbers!")
+            return []
+
+    def find_minimum(self):
+        numbers = self._get_numbers()
+        if numbers:
+            min_val = min(numbers)
+            self.result_label.config(text=f"Result: Minimum value is {min_val}")
+
+    def find_maximum(self):
+        numbers = self._get_numbers()
+        if numbers:
+            max_val = max(numbers)
+            self.result_label.config(text=f"Result: Maximum value is {max_val}")
+
+    def find_mode(self):
+        numbers = self._get_numbers()
+        if numbers:
+            from collections import Counter
+            count = Counter(numbers)
+            mode_data = dict(count)
+            mode = [k for k, v in mode_data.items() if v == max(list(count.values()))]
+
+            if len(mode) == len(numbers):
+                self.result_label.config(text=f"Result: No mode found")
+            else:
+                self.result_label.config(text=f"Result: Mode is {', '.join(map(str, mode))}")
+
 
 
 if __name__ == "__main__":
