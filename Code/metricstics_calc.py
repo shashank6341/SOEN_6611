d52b8b4ac50409fa2bb87b35063424483a47d93e
Shashank Verma <8446697+shashank6341@users.noreply.github.com>
2023-11-02 14:33:08 -0400

    Fixes for Text Validation
    Fixed an issue where it wont allow clearing of all the elements entered.

--- Code/metricstics_calc.py
+++ Code/metricstics_calc.py
@@ -37,31 +37,13 @@ class App(tk.Tk):
 
         self.entry = CustomEntry(self, placeholder="Ex: 1,45,57")
         self.entry.pack(fill=tk.BOTH, padx=10, pady=10)
-
-        self.entry.bind("<KeyRelease>", self._validate_input)
+        
         self.bind("<Button-1>", self.on_window_click)
 
     def on_window_click(self, event):
         widget = self.winfo_containing(event.x_root, event.y_root)
         if widget != self.entry:
             self.focus_set()
-
-    def _validate_input(self, _):
-        values = self.entry.get().split(',')
-        cleaned_values = []
-        for value in values:
-            value = value.strip()
-            if self._is_valid_number(value) or value == "":
-                cleaned_values.append(value)
-        self.entry.delete(0, tk.END)
-        self.entry.insert(0, ','.join(cleaned_values))
-
-    def _is_valid_number(self, value):
-        try:
-            float(value)
-            return True
-        except ValueError:
-            return False
 
 
 if __name__ == "__main__":
