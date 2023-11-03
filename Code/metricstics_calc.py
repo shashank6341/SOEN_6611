f9de3920aadd5835a8a00da4c6dbd542fa64df31
Shashank Verma <8446697+shashank6341@users.noreply.github.com>
2023-11-02 21:33:33 -0400

    Fixes for UI Lags #6
    - Implemented Threading for UI Fixes.

--- Code/metricstics_calc.py
+++ Code/metricstics_calc.py
@@ -1,12 +1,14 @@
+import threading
 import tkinter as tk
+from tkinter import messagebox
 
 
 class CustomEntry(tk.Entry):
-    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
-        super().__init__(master)
+    def __init__(self, master=None, placeholder="PLACEHOLDER", placeholder_color='grey', **kwargs):
+        super().__init__(master, **kwargs)
 
         self.placeholder = placeholder
-        self.placeholder_color = color
+        self.placeholder_color = placeholder_color
         self.default_fg_color = self['fg']
 
         self.bind("<FocusIn>", self._on_focus_in)
@@ -38,44 +40,39 @@ class App(tk.Tk):
         for i in range(3):  # Since you have 3 columns
             self.columnconfigure(i, weight=1)
 
-
         self.entry = CustomEntry(self, placeholder="Ex: 1,45,57")
         self.entry.grid(row=0, column=0, columnspan=3, padx=10, pady=5, sticky='ew')
-        
+
         self.bind("<Button-1>", self.on_window_click)
 
         # Result Label.
         self.result_label = tk.Label(self, text="Result: ")
         self.result_label.grid(row=1, column=0, columnspan=3, padx=10, pady=5, sticky='w')
 
-
         # Calculation Buttons
 
         # Using "ew" parameter to expand the UI Elements, East - West.
 
-        self.min_button = tk.Button(self, text="Minimum", command=self.find_minimum)
+        self.min_button = tk.Button(self, text="Minimum", command=lambda: self.calculate(self.find_minimum))
         self.min_button.grid(row=2, column=0, padx=5, pady=5, sticky='ew')
 
-        self.max_button = tk.Button(self, text="Maximum", command=self.find_maximum)
+        self.max_button = tk.Button(self, text="Maximum", command=lambda: self.calculate(self.find_maximum))
         self.max_button.grid(row=2, column=1, padx=5, pady=5, sticky='ew')
 
-        self.mode_button = tk.Button(self, text="Mode", command=self.find_mode)
+        self.mode_button = tk.Button(self, text="Mode", command=lambda: self.calculate(self.find_mode))
         self.mode_button.grid(row=2, column=2, padx=5, pady=5, sticky='ew')
 
-        self.median_btn = tk.Button(self, text="Median", command=self.find_median)
+        self.median_btn = tk.Button(self, text="Median", command=lambda: self.calculate(self.find_median))
         self.median_btn.grid(row=3, column=0, padx=5, pady=5, sticky='ew')
 
-        self.mean_btn = tk.Button(self, text="Arithmetic Mean", command=self.find_mean)
+        self.mean_btn = tk.Button(self, text="Arithmetic Mean", command=lambda: self.calculate(self.find_mean))
         self.mean_btn.grid(row=3, column=1, padx=5, pady=5, sticky='ew')
 
-        self.mad_button = tk.Button(self, text="Mean Absolute Deviation", command=self.calculate_mad)
+        self.mad_button = tk.Button(self, text="Mean Absolute Deviation", command=lambda: self.calculate(self.calculate_mad))
         self.mad_button.grid(row=4, column=0, padx=5, pady=5, sticky='ew')
 
-        self.sd_button = tk.Button(self, text="Standard Deviation", command=self.calculate_sd)
+        self.sd_button = tk.Button(self, text="Standard Deviation", command=lambda: self.calculate(self.calculate_sd))
         self.sd_button.grid(row=4, column=1, padx=5, pady=5, sticky='ew')
-
-
-
 
     def on_window_click(self, event):
         widget = self.winfo_containing(event.x_root, event.y_root)
@@ -90,51 +87,46 @@ class App(tk.Tk):
             tk.messagebox.showerror("Error", "Please enter valid comma-separated numbers!")
             return []
 
-    def find_minimum(self):
+    def calculate(self, func):
+        threading.Thread(target=self._calculate, args=(func,)).start()
+
+    def _calculate(self, func):
         numbers = self._get_numbers()
         if numbers:
-            min_val = min(numbers)
-            self.result_label.config(text=f"Result: Minimum value is {min_val}")
-
-    def find_maximum(self):
-        numbers = self._get_numbers()
-        if numbers:
-            max_val = max(numbers)
-            self.result_label.config(text=f"Result: Maximum value is {max_val}")
-
-    def find_mode(self):
-        numbers = self._get_numbers()
-        if numbers:
-            from collections import Counter
-            count = Counter(numbers)
-            mode_data = dict(count)
-            mode = [k for k, v in mode_data.items() if v == max(list(count.values()))]
-
-            if len(mode) == len(numbers):
-                self.result_label.config(text=f"Result: No mode found")
-            else:
-                self.result_label.config(text=f"Result: Mode is {', '.join(map(str, mode))}")
-
-    def find_median(self):
-        values = self._get_numbers()
-        if not values:
-            return
-        values.sort()
-        mid = len(values) // 2
-        if len(values) % 2 == 0:
-            median = (values[mid - 1] + values[mid]) / 2
+            result = func(numbers)
+            self.result_label.config(text=f"Result: {result}")
         else:
-            median = values[mid]
-        self.result_label.config(text=f"Result: Median is {median}")
+            tk.messagebox.showerror("Error", "Please enter valid comma-separated numbers!")
 
-    def find_mean(self):
-        values = self._get_numbers()
-        if not values:
-            return
-        mean = sum(values) / len(values)
-        self.result_label.config(text=f"Result: Arithmetic Mean is {mean}")
+    def find_minimum(self, numbers):
+        return f"{min(numbers)} (Minimum)"
 
+    def find_maximum(self, numbers):
+        return f"{max(numbers)} (Maximum)"
 
+    def find_mode(self, numbers):
+        from collections import Counter
+        count = Counter(numbers)
+        mode_data = dict(count)
+        mode = [k for k, v in mode_data.items() if v == max(list(count.values()))]
+
+        if len(mode) == len(numbers):
+            return "No mode found"
+        else:
+            return f"{', '.join(map(str, mode))} (Mode)"
+
+    def find_median(self, numbers):
+        numbers.sort()
+        mid = len(numbers) // 2
+        if len(numbers) % 2 == 0:
+            median = (numbers[mid - 1] + numbers[mid]) / 2
+        else:
+            median = numbers[mid]
+        return f"{median} (Median)"
+
+    def find_mean(self, numbers):
+        mean = sum(numbers) / len(numbers)
+        return f"{mean} (Arithmetic Mean)"
 
     def mean_absolute_deviation(self, numbers):
         mean = sum(numbers) / len(numbers)
@@ -144,24 +136,14 @@ class App(tk.Tk):
         mean = sum(numbers) / len(numbers)
         variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
         return variance ** 0.5
-    
-    def calculate_mad(self):
-        numbers = self._get_numbers()
-        if numbers:
-            result = self.mean_absolute_deviation(numbers)
-            self.result_label.config(text=f"Result: Mean Absolute Deviation is {result:.2f}")
-        else:
-            tk.messagebox.showerror("Error", "Please enter valid comma-separated numbers!")
 
-    def calculate_sd(self):
-        numbers = self._get_numbers()
-        if numbers:
-            result = self.standard_deviation(numbers)
-            self.result_label.config(text=f"Result: Standard Deviation is {result:.2f}")
-        else:
-            tk.messagebox.showerror("Error", "Please enter valid comma-separated numbers!")
+    def calculate_mad(self, numbers):
+        result = self.mean_absolute_deviation(numbers)
+        return f"{result:.2f} (Mean Absolute Deviation)"
 
-
+    def calculate_sd(self, numbers):
+        result = self.standard_deviation(numbers)
+        return f"{result:.2f} (Standard Deviation)"
 
 
 if __name__ == "__main__":
