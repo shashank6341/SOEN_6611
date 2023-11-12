a103e9426b8b79c80e9efa8195c7615d75b56f50
Wenxue Zhao <ballista01@outlook.com>
2023-11-12 17:43:42 -0500

    save and load history implemented

--- Code/calc.py
+++ Code/calc.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from statistics import Statistics
+import csv
 
 # Create a window
 window = tk.Tk()
@@ -11,11 +12,11 @@ frame.pack()
 
 # Create a label to display the instructions
 label = tk.Label(frame, text="Enter the data separated by commas:")
-label.grid(row=0, column=0, columnspan=2)
+label.grid(row=0, column=0, columnspan=3)
 
-# Create an entry to get the data from the user
-entry = tk.Entry(frame)
-entry.grid(row=1, column=0, columnspan=2)
+# Create a text widget to get the data from the user
+text = tk.Text(frame, height=5, width=40)
+text.grid(row=1, column=0, columnspan=3)
 
 # Create a list of statistics options
 options = ["Min", "Max", "Mode", "Median", "Mean", "MAD", "Stdev", "Variance"]
@@ -39,8 +40,8 @@ for i in range(len(options)):
 
 # Define a function to calculate and display the statistics
 def calculate():
-    # Get the data from the entry
-    data = entry.get()
+    # Get the data from the text widget
+    data = text.get("1.0", "end-1c")
     # Convert the data to a list of numbers
     data = [float(x) for x in data.split(",")]
     # Create a Statistics object
@@ -61,9 +62,62 @@ def calculate():
             # Clear the result
             labels[i].config(text="")
 
+# Define a function to restore the data from the history file
+def restore():
+    # Open the history file
+    with open("history.csv", "r") as file:
+        # Read the csv reader
+        reader = csv.reader(file)
+        # Get the last row
+        for row in reader:
+            pass
+        # Get the data from the last row
+        data = row[0]
+        # Insert the data into the text widget
+        text.delete("1.0", "end")
+        text.insert("1.0", data)
+
+# Define a function to clear the data and the results
+def clear():
+    # Delete the data from the text widget
+    text.delete("1.0", "end")
+    # Loop through the options
+    for i in range(len(options)):
+        # Uncheck the option
+        vars[i].set(0)
+        # Clear the result
+        labels[i].config(text="")
+
+# Define a function to save the data and the results to the history file
+def save():
+    # Get the data from the text widget
+    data = text.get("1.0", "end-1c")
+    # Get the results from the labels
+    results = []
+    for i in range(len(options)):
+        results.append(labels[i].cget("text"))
+    # Open the history file
+    with open("history.csv", "a") as file:
+        # Create a csv writer
+        writer = csv.writer(file)
+        # Write the data and the results as a row
+        writer.writerow([data] + results)
+
 # Create a button to trigger the calculation
-button = tk.Button(frame, text="Calculate", command=calculate)
-button.grid(row=10, column=0, columnspan=2)
+button_calculate = tk.Button(frame, text="Calculate", command=calculate)
+button_calculate.grid(row=10, column=0)
+
+# Create a button to trigger the restoration
+button_restore = tk.Button(frame, text="Restore", command=restore)
+button_restore.grid(row=10, column=1)
+
+# Create a button to trigger the clearing
+button_clear = tk.Button(frame, text="Clear", command=clear)
+button_clear.grid(row=10, column=2)
+
+# Create a button to trigger the saving
+button_save = tk.Button(frame, text="Save", command=save)
+button_save.grid(row=11, column=0, columnspan=3)
 
 # Start the main loop
 window.mainloop()
